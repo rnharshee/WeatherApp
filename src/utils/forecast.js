044e0adf6a720b3bdbdd34482529b53676ed1317
Harshita-Raghu <harshita.raghu@nividapps.com>
2020-01-16 18:28:03 +0530

    Added temphigh and templow

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@
 const request=require('request')
 const forecast=(latitude,longitude,callback)=>{
     const url='https://api.darksky.net/forecast/d47edb479c0e88a9645b358702f44fd3/'+latitude+','+longitude+'?units=si'
-    request({url,json:true},(error,{body})=>{
+    request({url,json:true},(error,{body}={})=>{
         if(error){
             callback('Unable to connect to weather service',undefined)
         }
@@ -12,7 +12,7 @@ const forecast=(latitude,longitude,callback)=>{
         else{
         const currently=body.currently
         const data=body.daily.data[0].summary+' It is currently '+ currently.temperature+' degrees out.There is a '+currently.precipProbability +'% chance of rain.'+
-        'Maximum temperature is '+body.daily.data[0].temperatureMax+' degrees. Minimum temperature is '+body.daily.data[0].temperatureMin+' degrees.'
+        'Temperature high today is '+body.daily.data[0].temperatureHigh+' degrees. Temperature low today is '+body.daily.data[0].temperatureLow+' degrees.'
         callback(undefined,data)
     }
 
